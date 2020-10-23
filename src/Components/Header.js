6a3ccdd846f6c101a14b885370d063a67b690eba
Ania <ania@Macieks-MacBook-Pro.local>
2020-10-23 15:49:38 +0200

    Add new components

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+class Header extends React.Component {
+    render () {
+        return (
+            <div class = "header">
+                <input class = "header__input"></input>
+            </div>
+        )
+    }
+}
+
+
+export default Header;
