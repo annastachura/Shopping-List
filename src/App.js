522f4adefd07a0ebc7d7f61d02c39cf86aa66f0e
Ania <ania@Macieks-MacBook-Pro.local>
2020-10-23 16:04:30 +0200

    New changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from "./Components/Header";
+import ShopList from "./Components/ShopList";
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
+      <div>
       <Header></Header>
+      <ShopList></ShopList>
+      </div>
     )
   }
 }
--- src/Components/ShopList.js
+++ src/Components/ShopList.js
@@ -1,1 +1,15 @@
-Import React from "react";
+import React from "react";
+
+class ShopList extends React.Component {
+    render() {
+        return (
+            <ul class = "shopList">
+                <li class = "shoplist__item">pomidor</li>
+                <li class = "shoplist__item">cola</li>
+                <li class = "shoplist__item">papier</li>
+            </ul>
+        )
+    }
+}
+
+export default ShopList;
