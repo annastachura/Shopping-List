96e98e978f1abb334a7fe866275d71d2ea56db3d
Ania <ania@Macieks-MacBook-Pro.local>
2020-10-27 11:12:49 +0100

    Add new scss look

--- src/Components/ShopList.js
+++ src/Components/ShopList.js
@@ -2,11 +2,13 @@ import React from "react";
 
 class ShopList extends React.Component {
     render() {
+        const {shoppingList} = this.props;
+        const listItems = shoppingList.map((item) => {
+            return <li class = "shoplist__item">{item}</li>
+        });
         return (
-            <ul class = "shopList">
-                <li class = "shoplist__item">pomidor</li>
-                <li class = "shoplist__item">cola</li>
-                <li class = "shoplist__item">papier</li>
+            <ul class = "shoplist">
+             {listItems}
             </ul>
         )
     }
