3a7a9463eda8985b880596757bdc5217ae28821f
Ania <ania@Macieks-MacBook-Pro.local>
2020-10-28 12:12:25 +0100

    Add new component

---
+++ src/Components/AlreadyBought.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+class AlreadyBought extends React.Component {
+    render() {
+        return (
+            <div class = "bought">
+                <ul class = "bought__list">
+                    <li class = "bought__item"></li>
+                    <li class = "bought__item"></li>
+                    <li class = "bought__item"></li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default AlreadyBought;
