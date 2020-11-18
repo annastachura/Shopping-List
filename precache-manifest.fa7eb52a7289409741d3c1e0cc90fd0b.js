003b117c0b11bb95a749e2ac652d64b35146e59e
Anna Czekańska <ania@Macieks-MacBook-Pro.local>
2020-11-18 11:26:56 +0100

    Updates

---
+++ precache-manifest.fa7eb52a7289409741d3c1e0cc90fd0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9910bc456e500046bc641c47f3edc16a",
+    "url": "/Shopping-List/index.html"
+  },
+  {
+    "revision": "940c11f49bd4aee7275e",
+    "url": "/Shopping-List/static/css/main.dc3fb1be.chunk.css"
+  },
+  {
+    "revision": "1f597ef4113bd1ecb32f",
+    "url": "/Shopping-List/static/js/2.23920493.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/Shopping-List/static/js/2.23920493.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "940c11f49bd4aee7275e",
+    "url": "/Shopping-List/static/js/main.01a80b45.chunk.js"
+  },
+  {
+    "revision": "4e3d877950528945b868",
+    "url": "/Shopping-List/static/js/runtime-main.d4253e81.js"
+  }
+]);
